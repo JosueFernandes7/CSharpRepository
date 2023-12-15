02e29d129e3002d9dd6278463b22f6de2f9fe94f
Josué Fernandes <josue7mf@hotmail.com>
2023-12-15 14:29:33 -0300

    feat: Enums Csharp

---
+++ Enums/Program.cs
@@ -0,0 +1,21 @@
+﻿using Enums.Entities;
+using Enums.Entities.Enums;
+namespace Enums
+{   
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1,
+                Moment = DateTime.Now,
+                Status = OrderStatus.Delivered
+            };
+
+            Console.WriteLine(order);
+        }
+
+    }
+}
