04287ca2e1cc8111e06d846549151f6e2e47729f
Josué Fernandes <josue7mf@hotmail.com>
2023-12-16 00:02:04 -0300

    feat: composição OOP

---
+++ Enums/Entities/Worker.cs
@@ -0,0 +1,68 @@
+﻿using Enums.Entities.Enums;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums.Entities
+{
+    class Worker
+    {
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+        public List<HourContract> Contracts { get; set; }
+
+        public Worker()
+        {
+            Contracts = new List<HourContract>();
+        }
+
+        public Worker(string name, WorkerLevel level, double baseSalary, Department department)
+        {
+            Name = name;
+            Level = level;
+            BaseSalary = baseSalary;
+            Department = department;
+            Contracts = new List<HourContract>();
+        }
+
+        public double Income(int year, int month)
+        {
+            double sum = BaseSalary;
+            foreach (HourContract contract in Contracts)
+            {
+                if (contract.Date.Year == year && contract.Date.Month == month)
+                    sum += contract.TotalValue();
+
+            }
+            return sum;
+        }
+
+        public void AddContract(HourContract contract)
+        {
+            if (!Contracts.Contains(contract))
+                Contracts.Add(contract);
+        }
+
+        public void RemoveContract(HourContract contract)
+        {
+            if (Contracts.Contains(contract))
+                Contracts.Remove(contract);
+        }
+
+        public int ShowContracts()
+        {
+            return Contracts.Count;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {this.Name}, Level: {this.Level}, Base Salary: {this.BaseSalary}, Departament: {this.Department.Name}, Contracts: {this.ShowContracts()}";
+        }
+    }
+}
--- Enums/Program.cs
+++ Enums/Program.cs
@@ -1,20 +1,49 @@
 ﻿using Enums.Entities;
 using Enums.Entities.Enums;
+using System.Globalization;
 namespace Enums
-{   
+{
 
     class Program
     {
         static void Main(string[] args)
         {
-            Order order = new Order
-            {
-                Id = 1,
-                Moment = DateTime.Now,
-                Status = OrderStatus.Delivered
-            };
+            Department department = new Department();
+            Worker worker = new Worker();
 
-            Console.WriteLine(order);
+            Console.Write("Enter deparment's name: ");
+            department.Name = Console.ReadLine();
+
+            Console.WriteLine("Enter worker data: ");
+            Console.Write("Name: ");
+            worker.Name = Console.ReadLine();
+            Console.Write("Level (Junior,MidLevel,Senior): ");
+            worker.Level = Enum.Parse<WorkerLevel>(Console.ReadLine());
+            Console.Write("Base Salary: ");
+            worker.BaseSalary = double.Parse(Console.ReadLine());
+            Console.Write("How many contracts to this worker? ");
+            int contracts = int.Parse(Console.ReadLine());
+            int i = 1;
+            while (contracts != 0)
+            {
+                HourContract hourContract = new HourContract();
+                Console.WriteLine($"Enter #{i} contract data:");
+                Console.Write("Date (DD/MM/YYYY): ");
+                hourContract.Date = DateTime.Parse(Console.ReadLine());
+                Console.Write("Value per hour: ");
+                hourContract.ValuePerHour = double.Parse(Console.ReadLine());
+                Console.Write("Duration (hours): ");
+                hourContract.Hours = int.Parse(Console.ReadLine());
+
+                worker.AddContract(hourContract);
+                i++;
+                contracts--;
+            }
+
+            Console.Write("Enter a month and year to calculate income (MM/YYYY): ");
+            DateTime data = DateTime.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.WriteLine($"Name {worker.Name} \nDepartment: {department.Name}\nIncome for {data.Month}/{data.Year}: {worker.Income(data.Year,data.Month).ToString("F2")}");
+
         }
 
     }
