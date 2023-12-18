48c5d09e45e62bf7161cfbb5173748db66fcbabe
Josué Fernandes <josue7mf@hotmail.com>
2023-12-18 20:08:06 -0300

    feat: file e fileinfo

---
+++ Arquivos/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Arquivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\Windows\Temp\file1.txt";
+            string targetPath = @"C:\Windows\Temp\file2.txt";
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                //fileInfo.CopyTo(targetPath);
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+
+            } catch (IOException e) {
+                Console.WriteLine("An error ocurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
