73f0ee898083efaacc827f4c757ebafbcc5cdb3b
Josué Fernandes <josue7mf@hotmail.com>
2023-12-18 14:47:06 -0300

    feat: virtual e override

---
+++ Heranca/Program.cs
@@ -0,0 +1,18 @@
+﻿using Heranca.Entities;
+
+namespace Heranca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account = new Account(1001,"Nelio",0.0); // classe pai
+
+            account.Withdraw(200);
+            BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500); // classe filha
+            bacc.Withdraw(200);
+
+
+        }
+    }
+}
