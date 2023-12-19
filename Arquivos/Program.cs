1e9c3f3795d9bdd84b1cb713e60380ffe905c7b8
Josué Fernandes <josue7mf@hotmail.com>
2023-12-18 22:20:00 -0300

    feat: Windows Forms, media calculator

--- Arquivos/Program.cs
+++ Arquivos/Program.cs
@@ -7,26 +7,6 @@
             string sourcePath = @"C:\Windows\Temp\file1.txt";
             StreamReader sr = null;
 
-
-            try
-            {
-                
-                sr = File.OpenText(sourcePath);
-                while (!sr.EndOfStream)
-                {
-                    Console.WriteLine(sr.ReadLine());
-                }
-            }
-            catch (IOException e)
-            {
-                Console.WriteLine("An error ocurred");
-                Console.WriteLine(e.Message);
-            }
-            finally
-            {
-                if (sr != null)
-                    sr.Close();
-            }
         }
     }
 }
