d07eccb349631ca8149d51c4d57019b212f9889a
Josué Fernandes <josue7mf@hotmail.com>
2023-12-28 10:15:22 -0300

    feat: generics

--- Heranca/obj/Release/net8.0/Heranca.AssemblyInfo.cs
+++ Heranca/obj/Release/net8.0/Heranca.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("HerancaPolimorfismoExercicio")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Heranca")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("HerancaPolimorfismoExercicio")]
-[assembly: System.Reflection.AssemblyTitleAttribute("HerancaPolimorfismoExercicio")]
+[assembly: System.Reflection.AssemblyProductAttribute("Heranca")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Heranca")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
